1376726cca1457e84dc9f4910691c67b976d5525
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-07 09:56:32 +0530

    peak and pop method

---
+++ DataStructureProgram/DataStructureProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace DataStructureProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Solving Problems on Stack and Queue");
+
+            LinkedListStack obj = new LinkedListStack();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+
+            obj.Display();
+
+        }
+    }
+}
